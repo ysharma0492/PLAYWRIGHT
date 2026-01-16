721e70559f5702ffe8fbba4fec73c64eb00ce6ff
yogesh sharma <ysharma0492@gmail.com>
2026-01-17 03:34:10 +0530

    forever

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,22 +13,28 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+
   /* Run tests in files in parallel */
   fullyParallel: true,
+
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "allure-playwright",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [
+    ['html', { open: 'never' }],
+    ['allure-playwright']
+  ],
+
+  /* Shared settings for all the projects below. */
+  use: {
+    /* Collect trace when retrying the failed test */
     trace: 'on-first-retry',
     headless: true,
   },
@@ -37,8 +43,11 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'],headless: true,viewport: { width: 1536, height: 730 } },
-      
+      use: {
+        ...devices['Desktop Chrome'],
+        headless: true,
+        viewport: { width: 1536, height: 730 },
+      },
     },
 
     {
